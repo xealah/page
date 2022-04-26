bac06141a8c38c2391a1bb6c403fb42d7bb231d7
xealah <102432420+xealah@users.noreply.github.com>
2022-04-26 19:45:21 +0100

    Add files via upload

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <h1 class="centered" v-if="this.error.statusCode == 404">
+      Oops. That page doesn't exist.
+    </h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+
+.centered {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: fixed;
+}
+</style>
